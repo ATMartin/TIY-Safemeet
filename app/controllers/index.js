0a1de49f3370292b05ec0938e568b845c702eff5
Alex Martin <martin.alex.t@gmail.com>
2015-03-13 10:53:02 -0400

    Implement HTML5 Geolocation awareness & live updates to index map

---
+++ app/controllers/index.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  latitude: 34,
+  longitude: -84,
+  actions: {
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+afterModel: function() {
+//  navigator.geolocation.getCurrentPosition(function(data) { 
+//    console.log(data);
+    //this.controller.set('latitude', data.coords.latitude);
+    //this.controller.set('longitude', data.coords.longitude);
+},
+setupController: function(controller) { 
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(data) { 
+      console.log(data);
+      controller.set('latitude', data.coords.latitude);
+      controller.set('longitude', data.coords.longitude);
+    },
+    function() {
+      console.log("There was an error obtaining your current location. Falling back to default...");
+    });
+  } else {
+    console.log("Your browser does not support goelocation. Falling back to default location...");
+  }
+}
+
 });
